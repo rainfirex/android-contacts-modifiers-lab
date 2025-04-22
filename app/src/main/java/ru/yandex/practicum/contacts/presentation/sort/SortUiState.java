996b9a0db54b91b32dfec5cc7e2210a62be1ff57
Александр <rainfire491@gmail.com>
2025-04-22 18:27:08 +1100

    update

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortUiState.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortUiState.java
@@ -4,8 +4,23 @@ package ru.yandex.practicum.contacts.presentation.sort;
 public class SortUiState {
 
     // сделайте поля isApplyEnable и newSelectedSortType приватными
-    public boolean isApplyEnable = false;
-    public String newSelectedSortType = null;
+    boolean isApplyEnable = false;
+    String newSelectedSortType = null;
 
     // реализуйте get и set методы для обоих полей
+    public boolean getIsApplyEnable(){
+        return  isApplyEnable;
+    }
+
+    public void setIsApplyEnable(boolean applyEnable) {
+        isApplyEnable = applyEnable;
+    }
+
+    public String getNewSelectedSortType(){
+        return newSelectedSortType;
+    }
+
+    public void setNewSelectedSortType(String newSelectedSortType) {
+        this.newSelectedSortType = newSelectedSortType;
+    }
 }
